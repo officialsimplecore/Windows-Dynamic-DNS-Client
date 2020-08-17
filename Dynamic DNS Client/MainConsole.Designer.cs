a481e79dd02a8ebc99164aed3efc6d5e7f650b24
Justin V. - SC <justin@simplecore.org>
2020-08-16 19:38:40 -0700

    Adjust window size
    Makes smaller.

--- Dynamic DNS Client/MainConsole.Designer.cs
+++ Dynamic DNS Client/MainConsole.Designer.cs
@@ -233,7 +233,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 375);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label12);
